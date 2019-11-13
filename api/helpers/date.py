39008e6f3df36173fa9f2985f6717fe7447b599b
victorhundo <victorhundo@gmail.com>
2019-11-13 07:49:39 -0300

    Date and docker

--- api/helpers/date.py
+++ api/helpers/date.py
@@ -7,4 +7,5 @@ def time_br(time):
         end_time += '.000000'
     d = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S.%f")
     FULL_MONTHS = {1:'janeiro',  2:'fevereiro', 3:u'março',4:'abril', 5:'maio', 6:'junho', 7:'julho', 8:'agosto', 9:'setembro', 10:'outubro',  11:'novembro', 12:'dezembro'}
-    return u"%s de %s às %s:%s" % (d.day,FULL_MONTHS[d.month],d.hour,d.minute)
+    return u"%s de %s às %s:%s" % (d.day,FULL_MONTHS[d.month],d.hour - 3,d.minute)
+    # FixME: d.hour -3 porque timzone django não seta para America/Recife
